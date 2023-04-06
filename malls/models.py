8af627d959229a60872c7310bd4a3253dde197e6
mintropy <jun2korea@gmail.com>
2023-04-06 20:37:24 +0900

    Feat: Migration for Mall and User

--- malls/models.py
+++ malls/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
+from users.models import User
+
 # Create your models here.
 
 
 class Mall(models.Model):
     name = models.CharField(max_length=50)
+    user = models.ForeignKey(
+        User,
+        null=True,
+        on_delete=models.SET_NULL,
+    )
