e9eb3ada121252504f4e6771f5b5ee23915bd61f
mintropy <jun2korea@gmail.com>
2023-04-06 20:30:18 +0900

    Feat: User model

--- main/settings.py
+++ main/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     # my app
+    "users",
     "malls",
     # third party app
     "rest_framework",
@@ -126,3 +127,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "users.User"
